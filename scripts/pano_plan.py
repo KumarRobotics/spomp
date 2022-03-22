35d751e07a45ebefc81d2fa785b5a4d67d85d437
Ian D. Miller <iandouglas96@gmail.com>
2022-03-21 20:21:27 -0400

    Publishing to ros, lots of tuning

--- scripts/pano_plan.py
+++ scripts/pano_plan.py
@@ -6,6 +6,8 @@ import numpy as np
 from vedo import Points, show
 from scipy import ndimage
 import cv2
+import rospy
+from sensor_msgs.msg import PointCloud2, PointField
 from skimage import morphology
 from sensor_msgs.msg import Image
 
@@ -16,13 +18,21 @@ class PanoPlan:
         self.bag_ = rosbag.Bag(bag)
         self.vfov_ = np.deg2rad(90.)
 
+        rospy.init_node('pano_plan')
+        self.viz_pub_ = rospy.Publisher('~viz', PointCloud2, queue_size=1)
+
     def parse(self, num_panos = np.inf, start_ind = 0):
         count = 0
+        rate = rospy.Rate(10)
         for topic, msg, t in self.bag_.read_messages():
+            if rospy.is_shutdown():
+                break
             if topic == '/os_node/llol_odom/pano':
                 data = np.frombuffer(msg.data, dtype=np.uint16).reshape(msg.height, msg.width, -1)
                 if count >= start_ind:
                     self.parse_pano(data)
+                    print(count)
+                    rate.sleep()
                 count += 1
                 if count >= num_panos + start_ind:
                     break
@@ -30,12 +40,37 @@ class PanoPlan:
     def parse_pano(self, pano):
         dists = pano[:, :, 0].astype(np.float32) / 512.
 
-        #fill holes
-
+        #helpful LUTs
         elev_delta = self.vfov_ / (pano.shape[0] - 1)
         azi_delta = np.pi * 2 / pano.shape[1]
         elevs = np.arange(self.vfov_/2, -self.vfov_/2-0.0001, -elev_delta)
         azis = np.arange(0, np.pi*2, azi_delta)
+        
+        #fill holes
+        for row_ind, row in enumerate(dists):
+            if elevs[row_ind] > np.deg2rad(-5):
+                #ignore the top half
+                continue
+            nonzero_inds = np.nonzero(row)[0]
+            if len(nonzero_inds) < len(row)/10:
+                # not meaningful enough to fill in
+                dists[row_ind, :] = 0 
+                continue
+            for ind, dist in enumerate(row):
+                if dist == 0:
+                    before = nonzero_inds[nonzero_inds < ind]
+                    after = nonzero_inds[nonzero_inds > ind]
+                    if len(before) == 0:
+                        if after[0] < 10:
+                            dists[row_ind, ind] = row[after[0]]
+                    elif len(after) == 0:
+                        if len(row) - before[-1] < 10:
+                            dists[row_ind, ind] = row[before[-1]]
+                    else:
+                        #linear interp
+                        if after[0] - before[-1] < 10:
+                            dists[row_ind, ind] = ((ind - before[-1]) * row[after[0]] + (after[0] - ind) * row[before[-1]]) / (after[0] - before[-1])
+
         zs = np.sin(elevs)[:, None] * dists
         ranges = np.cos(elevs)[:, None] * dists
         xs = np.cos(azis) * ranges
@@ -47,7 +82,8 @@ class PanoPlan:
         alts = zs.copy()
         alts[alts == 0] = np.nan
 
-        smoothed_dist = np.zeros(alts.shape, dtype=np.float32)
+        smoothed_dist = np.empty(alts.shape, dtype=np.float32)
+        smoothed_dist[:] = np.nan
         for ind, row in enumerate(dists):
             avg_dist = np.nanmedian(ranges[ind, :])
             if np.isnan(avg_dist):
@@ -55,46 +91,106 @@ class PanoPlan:
             window_size = 0.5 / (avg_dist * azi_delta)
             smoothed_dist[ind, :] = ndimage.median_filter(row, np.maximum(1, int(window_size)))
 
-        alt_delta = np.maximum(np.abs(np.roll(smoothed_dist, -2, axis=1) - np.roll(smoothed_dist, 2, axis=1)) - 0.03, 0) / (azi_delta * ranges**2) * np.abs(np.sin(elevs[:, None]))
+            #median_filter doesn't handle nans properly
+            nans = np.isnan(row)
+            nans_d = cv2.dilate(nans.astype(np.uint8), np.ones(int(window_size)))
+            smoothed_dist[ind, nans_d[:, 0]>0] = np.nan
+
+        alt_delta = np.maximum(np.abs(np.roll(smoothed_dist, -2, axis=1) - np.roll(smoothed_dist, 2, axis=1)) - 0.05, 0) / (azi_delta * ranges * 5) * np.abs(np.sin(elevs[:, None]))
 
         smoothed_vert_range = np.cos(elevs)[:, None] * smoothed_dist
         smoothed_vert_alt = np.sin(elevs)[:, None] * smoothed_dist
-        #smoothed_vert_alt = ndimage.median_filter(alts, (1, 1))
-        #smoothed_vert_range = ndimage.median_filter(ranges, (1, 1))
+        #smoothed_vert_alt = ndimage.median_filter(smoothed_vert_alt, (3, 5))
+        #smoothed_vert_range = ndimage.median_filter(smoothed_vert_range, (3, 5))
         #find short vertical obstacles
-        alt_delta_vert = np.maximum(np.abs((smoothed_vert_alt - np.roll(smoothed_vert_alt, 1, axis=0))) - 0.03 * (1 - np.abs(np.sin(elevs[:, None]))), 0) / \
-                np.abs(smoothed_vert_range - np.roll(smoothed_vert_range, 1, axis=0))
+        alt_delta_vert = np.maximum(np.abs((smoothed_vert_alt - np.roll(smoothed_vert_alt, 1, axis=0))) - 0.05 * np.abs(np.sin(elevs[:, None])), 0) / \
+                (np.abs(smoothed_vert_range - np.roll(smoothed_vert_range, 1, axis=0)) + 0.05*np.abs(np.cos(elevs[:, None])))
         #find shallower obstacles
-        alt_delta_vert += np.maximum(np.abs((smoothed_vert_alt - np.roll(smoothed_vert_alt, 3, axis=0))) - 0.03 * (1 - np.abs(np.sin(elevs[:, None]))), 0) / \
-                np.abs(smoothed_vert_range - np.roll(smoothed_vert_range, 3, axis=0))
+        alt_delta_vert += (np.maximum(np.abs((smoothed_vert_alt - np.roll(smoothed_vert_alt, 3, axis=0))) - 0.05 * np.abs(np.sin(elevs[:, None])), 0) / \
+                (np.abs(smoothed_vert_range - np.roll(smoothed_vert_range, 3, axis=0)) + 0.05*np.abs(np.cos(elevs[:, None]))))
 
-        obs = np.maximum(alt_delta, alt_delta_vert) > 0.3
+        obs = np.sqrt(alt_delta**2 + alt_delta_vert**2) > 0.25
         obs = np.nan_to_num(obs)
         obs = morphology.remove_small_objects(obs, 10)
 
-        #inflate along azimuth
         obs_inf = np.zeros(obs.shape, dtype=np.float32)
-        for ind, row in enumerate(obs):
+        #inflate along altitude
+        for col_ind, col in enumerate(obs.T):
+            initial_range = np.inf
+            for row_ind, pt in enumerate(col):
+                if pt > 0 and ranges[row_ind, col_ind] < initial_range:
+                    initial_range = ranges[row_ind, col_ind]
+                if initial_range != 0 and np.abs(initial_range - ranges[row_ind, col_ind]) < 0.5:
+                    obs_inf[row_ind, col_ind] = 1
+
+        #inflate along azimuth
+        for ind, row in enumerate(obs_inf):
             avg_dist = np.nanmedian(ranges[ind, :])
             if np.isnan(avg_dist):
                 continue
             window_size = 0.5 / (avg_dist * azi_delta)
             obs_inf[ind, :] = cv2.dilate(row[None, :].astype(np.uint8), np.ones([1, int(window_size)]))
-        #inflate along altitude
-        for row_ind, row in enumerate(obs.T):
-            initial_range = 0
-            for col_ind, pt in enumerate(row):
-                if pt > 0:
-                    initial_range = ranges[col_ind, row_ind]
-                if initial_range != 0 and np.abs(initial_range - ranges[col_ind, row_ind]) < 0.5:
-                    obs_inf[col_ind, row_ind] = 1
-        obs_inf = np.maximum(obs.astype(np.float32)*2, obs_inf)
 
-        pc_points = Points([xs.flatten(), ys.flatten(), zs.flatten()]).cmap("rainbow", obs_inf.flatten())
-        show(pc_points, resetcam=False, camera={'pos':(0,0,30)}).close()
+        obs_inf = np.maximum(obs.astype(np.float32)*3, obs_inf)
 
-        #self.viz_img(alts, 'alts')
-        #self.viz_img(smoothed_vert_alt, 'smoothed')
+        #find safe area, expanding from home
+        for col_ind, col in enumerate(obs_inf.T):
+            last_range = 0
+            for row_ind, pt in reversed(list(enumerate(col))):
+                if not np.isnan(dists[row_ind, col_ind]):
+                    dist = np.abs(last_range - ranges[row_ind, col_ind])
+                    if last_range == 0 or (dist < 1 and pt == 0):
+                        last_range = ranges[row_ind, col_ind]
+                        obs_inf[row_ind, col_ind] = 2
+                    elif last_range != 0:
+                        break
+
+        #VISUALIZATION
+        #ROS viz
+        viz_msg = PointCloud2()
+        viz_msg.header.frame_id = "map"
+
+        pos_field_x = PointField()
+        pos_field_x.name = 'x'
+        pos_field_x.offset = 0
+        pos_field_x.datatype = PointField.FLOAT32
+        pos_field_x.count = 1
+        pos_field_y = PointField()
+        pos_field_y.name = 'y'
+        pos_field_y.offset = 4
+        pos_field_y.datatype = PointField.FLOAT32
+        pos_field_y.count = 1
+        pos_field_z = PointField()
+        pos_field_z.name = 'z'
+        pos_field_z.offset = 8
+        pos_field_z.datatype = PointField.FLOAT32
+        pos_field_z.count = 1
+        pos_field_label = PointField()
+        pos_field_data = PointField()
+        pos_field_data.name = 'data'
+        pos_field_data.offset = 12
+        pos_field_data.datatype = PointField.FLOAT32
+        pos_field_data.count = 1
+
+        viz_msg.fields.append(pos_field_x)
+        viz_msg.fields.append(pos_field_y)
+        viz_msg.fields.append(pos_field_z)
+        viz_msg.fields.append(pos_field_data)
+
+        viz_msg.point_step = 16
+        viz_msg.height = 1
+        viz_msg.width = 0
+        viz_msg.width = xs.size
+        
+        stacked_data = np.vstack([xs.flatten(), -ys.flatten(), zs.flatten(), obs_inf.flatten()]).astype(np.float32).T
+        viz_msg.data = stacked_data.tobytes()
+        self.viz_pub_.publish(viz_msg)
+
+        #pc_points = Points([xs.flatten(), ys.flatten(), zs.flatten()]).cmap("rainbow", obs_inf.flatten())
+        #show(pc_points, resetcam=False, camera={'pos':(0,0,30)}).close()
+
+        #self.viz_img(dists, 'dists')
+        #self.viz_img(smoothed_dist, 'dists_smoothed')
         #self.viz_img(np.minimum(alt_delta, 1), 'alt_delta')
         #self.viz_img(np.minimum(alt_delta_vert, 1), 'alt_delta_vert')
         #self.viz_img(obs_inf, 'obs')
@@ -111,4 +207,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     pp = PanoPlan(args.bag)
-    pp.parse(start_ind = 1)
+    pp.parse(start_ind = 0)
