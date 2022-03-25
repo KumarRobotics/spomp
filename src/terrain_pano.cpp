d563ad38e5ce64ee2547e5eebbb7fd17dedd17d2
Ian D. Miller <iandouglas96@gmail.com>
2022-03-24 20:59:23 -0400

    Started terrain pano implementation

--- src/terrain_pano.cpp
+++ src/terrain_pano.cpp
@@ -6,10 +6,25 @@ TerrainPano::TerrainPano(const Params& params) : params_(params) {}
 
 void TerrainPano::updatePano(const Eigen::ArrayXXf& pano, 
     const Eigen::Isometry3f& pose) 
-{
-}
+{}
 
 void TerrainPano::fillHoles(Eigen::ArrayXXf& pano) const {
+  for (int row_i=0; row_i<pano.rows(); ++row_i) {
+    int first_nonzero = -1;
+    int last_nonzero = -1;
+    for (int col_i=0; col_i<pano.cols(); ++col_i) {
+      if (pano(row_i, col_i) > 0) {
+        // Found a hole small enough to fill
+        if (last_nonzero - row_i > 1 && last_nonzero - row_i < 100) {
+        }
+        
+        last_nonzero = col_i;
+        if (first_nonzero < 0) {
+          first_nonzero = last_nonzero;
+        }
+      }
+    }
+  }
 }
 
 //! Compute the gradient across the panorama
--- test/bench.cpp
+++ test/bench.cpp
@@ -1,14 +1,29 @@
 #include <benchmark/benchmark.h>
+#include <string>
 
-static void BM_test(benchmark::State& state) {
+static void BM_mod(benchmark::State& state) {
   for (auto _ : state) {
-    int b = 0;
-    for (int a=0; a<1000; a++) {
-      b += a;
+    for (int a=0; a<100; a++) {
+      for (int b=0; b<a*2; b++) {
+        int res = b % a;
+        benchmark::DoNotOptimize(res);
+      }
     }
   }
 }
 
-BENCHMARK(BM_test);
+static void BM_mod_fast(benchmark::State& state) {
+  for (auto _ : state) {
+    for (int a=0; a<100; a++) {
+      for (int b=0; b<a*2; b++) {
+        int res = b <= a ? b : b - a;
+        benchmark::DoNotOptimize(res);
+      }
+    }
+  }
+}
+
+BENCHMARK(BM_mod);
+BENCHMARK(BM_mod_fast);
 
 BENCHMARK_MAIN();
