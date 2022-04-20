64fe50cf446e518ff116ebbe4ed69bb05edb00d7
Ian D. Miller <iandouglas96@gmail.com>
2022-04-19 23:21:38 -0400

    Started building mapper class with threads

--- include/spomp/mapper.h
+++ include/spomp/mapper.h
@@ -1,10 +1,29 @@
 #pragma once
 
+#include <thread>
+#include "spomp/pose_graph.h"
+
 namespace spomp {
 
 class Mapper {
   public:
     Mapper();
+
+  private:
+    std::thread pose_graph_thread_;
+    class PoseGraphThread {
+      public:
+        PoseGraphThread(Mapper& m, const PoseGraph& p) : 
+          mapper_(m), pg_(p) {}
+
+        bool operator()();
+
+      private:
+        // Reference back to parent
+        Mapper& mapper_;
+
+        PoseGraph pg_;
+    };
 };
 
 } // namespace spomp
