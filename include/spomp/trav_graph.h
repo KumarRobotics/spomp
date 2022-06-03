8b05fa2a0e28e6b370aa437bbda00fba8e774171
Ian D. Miller <iandouglas96@gmail.com>
2022-06-03 00:30:27 -0400

    Djikstra implementation (untested)

--- include/spomp/trav_graph.h
+++ include/spomp/trav_graph.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <list>
 #include <Eigen/Dense>
 
 namespace spomp {
@@ -14,23 +15,51 @@ class TravGraph {
     // Forward declaration
     struct Edge;
     struct Node {
-      std::vector<Edge*> edges;
-      
+      std::vector<Edge*> edges{};
       Eigen::Vector2f pos{0, 0};
+
+      // Graph-search temps
+      bool visited{false};
+      float cost{std::numeric_limits<float>::infinity()};
+      Edge* best_prev_edge{nullptr};
+
+      Node(const Eigen::Vector2f& p) : pos(p) {}
     };
 
     struct Edge {
-      Node* node1;
-      Node* node2;
+      Node* node1{nullptr};
+      Node* node2{nullptr};
 
       float cost{0};
       int cls{0};
+
+      Edge(Node* const n1, Node* const n2, float c, int cls) :
+        node1(n1), node2(n2), cost(c), cls(cls) {}
+
+      Node* getOtherNode(const Node* n) const {
+        if (n == node1) return node2;
+        return node1;
+      }
     };
 
-    std::vector<const Node*> getPath();
+    //! Djikstra shortest-path solver
+    std::list<const Node*> getPath(Node* start_n, Node* end_n);
+
+    void addNode(const Node& node) {
+      nodes_.push_back(node);
+    }
+
+    void addEdge(const Edge& edge);
 
   private:
+    /*********************************************************
+     * LOCAL FUNCTIONS
+     *********************************************************/
+    void reset();
 
+    /*********************************************************
+     * LOCAL VARIABLES
+     *********************************************************/
     std::vector<Node> nodes_;
     std::vector<Edge> edges_;
 };
--- src/trav_graph.cpp
+++ src/trav_graph.cpp
@@ -1,7 +1,77 @@
+#include <queue>
 #include "spomp/trav_graph.h"
 
 namespace spomp {
 
+TravGraph::TravGraph() {}
 
+std::list<const TravGraph::Node*> TravGraph::getPath(
+    Node* start_n, Node* end_n) 
+{
+  // Initial conditions
+  reset();
+  start_n->cost = 0;
+  start_n->visited = true;
+  // We need to make a custom comparison, or we will end up sorting by
+  // the pointers' order in mem
+  auto node_comp = [](Node* a, Node* b) { return a->cost < b->cost; };
+  std::priority_queue<Node*, std::vector<Node*>, decltype(node_comp)> node_q(node_comp);
+  node_q.emplace(start_n);
+
+  // Djikstra
+  while (node_q.size() > 0) {
+    Node* cur_n = node_q.top();
+    node_q.pop();
+    if (cur_n->visited) {
+      // lazy deletion
+      continue;
+    }
+    if (cur_n == end_n) {
+      // We got to the goal
+      end_n->visited = true;
+      break;
+    }
+
+    for (const auto& edge : cur_n->edges) {
+      Node* next_n = edge->getOtherNode(cur_n);
+      float new_cost = cur_n->cost + edge->cost;
+      if (new_cost < next_n->cost) {
+        next_n->cost = new_cost;
+        next_n->best_prev_edge = edge;
+        node_q.emplace(next_n);
+      }
+    }
+    cur_n->visited = true;
+  }
+
+  // Traverse the graph to actually find the path
+  std::list<const Node*> path;
+  if (end_n->visited) {
+    // We have found a path
+    path.push_front(end_n);
+    while (path.front() != start_n) {
+      path.push_front(path.front()->best_prev_edge->getOtherNode(path.front()));
+    }
+  }
+  return path;
+}
+
+void TravGraph::addEdge(const Edge& edge) {
+  if (edge.node1 && edge.node2) {
+    edges_.push_back(edge);
+
+    // Add edge to nodes
+    Edge* edge_ptr = &edges_.back();
+    edge.node1->edges.push_back(edge_ptr);
+    edge.node2->edges.push_back(edge_ptr);
+  }
+}
+
+void TravGraph::reset() {
+  for (auto& node : nodes_) {
+    node.visited = false;
+    node.cost = std::numeric_limits<float>::infinity();
+  }
+}
 
 } // namespace spomp
