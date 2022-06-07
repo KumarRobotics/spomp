18cd382b85582b1059cff4202f7bf70b73281ef0
Ian D. Miller <iandouglas96@gmail.com>
2022-06-07 12:04:07 -0400

    Started trav_map implementation, untested

--- include/spomp/trav_map.h
+++ include/spomp/trav_map.h
@@ -1,10 +1,39 @@
 #pragma once
 
+#include <opencv2/core.hpp>
+#include "spomp/trav_graph.h"
+
 namespace spomp {
 
 class TravMap {
   public:
-    TravMap();
+    struct Params {
+      std::string terrain_types_path = "";
+    };
+    TravMap(const Params& p);
+
+    void updateMap(const cv::Mat& map);
+
+    cv::Mat viz();
+
+  private:
+    /*********************************************************
+     * LOCAL FUNCTIONS
+     *********************************************************/
+    void loadTerrainLUT();
+
+    /*********************************************************
+     * LOCAL CONSTANTS
+     *********************************************************/
+    Params params_;
+
+    /*********************************************************
+     * LOCAL VARIABLES
+     *********************************************************/
+    cv::Mat terrain_lut_{};
+    cv::Mat map_{};
+    
+    TravGraph graph_{};
 };
 
 } // namespace spomp
--- src/trav_map.cpp
+++ src/trav_map.cpp
@@ -1,8 +1,37 @@
+#include <yaml-cpp/yaml.h>
 #include "spomp/trav_map.h"
 
 namespace spomp {
 
-TravMap::TravMap() {
+TravMap::TravMap(const Params& p) : params_(p) {
+  loadTerrainLUT();
+}
+
+void TravMap::loadTerrainLUT() {
+  terrain_lut_ = cv::Mat::ones(256, 1, CV_8UC1) * 255;
+  if (params_.terrain_types_path != "") {
+    const YAML::Node terrain_types = YAML::LoadFile(params_.terrain_types_path);
+    int terrain_id = 0;
+    for (const auto& terrain_type : terrain_types) {
+      if (terrain_type.as<int>() >= 0) {
+        // If less than 0, leave as untraversable
+        terrain_lut_.at<uint8_t>(terrain_id) = terrain_type.as<int>();
+      }
+      ++terrain_id;
+    }
+  } else {
+    // Default: assume first class is traversable, no others
+    terrain_lut_.at<uint8_t>(0) = 0;
+    terrain_lut_.at<uint8_t>(1) = 1;
+  }
+}
+
+void TravMap::updateMap(const cv::Mat &map) {
+  map_ = map;
+}
+
+cv::Mat TravMap::viz() {
+  return {};
 }
 
 } // namespace spomp
