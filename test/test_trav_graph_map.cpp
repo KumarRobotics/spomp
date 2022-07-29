3788b025f5ba7d040c6cc8482cb94cb00fa51e9b
Ian D. Miller <iandouglas96@gmail.com>
2022-07-28 21:56:43 -0400

    more testing

--- test/test_trav_graph_map.cpp
+++ test/test_trav_graph_map.cpp
@@ -108,9 +108,10 @@ TEST(trav_map, update_map) {
   m.updateMap(map_img, {-24.1119060516, 62.8522758484});
 
   Reachability reach{};
-  reach.proj = AngularProj(AngularProj::StartFinish{0, 2*pi}, 10);
-  reach.scan = Eigen::VectorXf::Zero(10);
-  reach.is_obs = Eigen::VectorXi::Ones(10);
+  reach.proj = AngularProj(AngularProj::StartFinish{0, 2*pi}, 360);
+  reach.scan = Eigen::VectorXf::Ones(360)*100;
+  reach.scan.tail<180>().setConstant(0);
+  reach.is_obs = Eigen::VectorXi::Ones(360);
   m.updateLocalReachability(reach, Eigen::Isometry2f::Identity());
 
   cv::imwrite("spomp_trav_updated_map.png", m.viz());
