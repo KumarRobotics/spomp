e886560b17d53ac6eeee1d192d08901a0d7f8008
Ian D. Miller <iandouglas96@gmail.com>
2023-01-13 19:16:35 -0500

    Can draw pretty lines

--- scripts/aerial_context.py
+++ scripts/aerial_context.py
@@ -3,8 +3,10 @@
 import numpy as np
 import cv2
 
+import rospy
 import rosbag
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
 
 class AerialMap:
     def __init__(self, path):
@@ -13,6 +15,10 @@ class AerialMap:
         self.sem_viz_ = None
         self.intermed_ = None
         self.center_ = np.array([0., 0])
+
+        self.trav_edges_ = []
+        self.new_edges_trav_ = True
+        self.last_pt_ = None
 
         for topic, msg, t in rosbag.Bag(path, 'r').read_messages():
             if 'Image' in str(msg.__class__):
@@ -37,11 +43,47 @@ class AerialMap:
                 self.center_[1] = msg.point.x
                 print("Loaded center")
 
-    def show_color(self):
-        cv2.imshow('color', self.color_)
-        cv2.imshow('sem_viz', self.sem_viz_)
-        cv2.waitKey(0)
+        self.map_pub_ = rospy.Publisher('~map', Image, queue_size=10)
+        self.map_click_sub_ = rospy.Subscriber('~map_mouse_left', Point, self.map_click_cb)
+
+        self.pub_timer_ = rospy.Timer(rospy.Duration(1), self.publish_map)
+
+    def get_trav_color(self, is_trav):
+        if is_trav:
+            return (0, 255, 0)
+        else:
+            return (0, 0, 255)
+
+    def publish_map(self, timer=None):
+        annotated_map = self.color_.copy()
+
+        cv2.rectangle(annotated_map, (0, 0), (20, 20), self.get_trav_color(self.new_edges_trav_), -1)
+        for edge in self.trav_edges_:
+            cv2.line(annotated_map, edge[1], edge[2], self.get_trav_color(edge[0]), 1)
+
+        map_msg = Image()
+        map_msg.encoding = "bgr8"
+        map_msg.height = annotated_map.shape[0]
+        map_msg.width = annotated_map.shape[1]
+        map_msg.step = map_msg.width * 3
+        map_msg.data = annotated_map.tobytes()
+        self.map_pub_.publish(map_msg)
+
+    def map_click_cb(self, click_pt_msg):
+        if click_pt_msg.x < 20 and click_pt_msg.y < 20:
+            self.new_edges_trav_ = not self.new_edges_trav_
+            self.publish_map()
+            return
+
+        pt = np.array([click_pt_msg.x, click_pt_msg.y], dtype=np.int16)
+        if self.last_pt_ is None:
+            self.last_pt_ = pt
+        else:
+            self.trav_edges_.append((self.new_edges_trav_, pt, self.last_pt_))
+            self.last_pt_ = None
+            self.publish_map()
 
 if __name__ == '__main__':
+    rospy.init_node("aerial_context_test")
     am = AerialMap('/media/ian/ResearchSSD/xview_collab/iapetus/twojackalquad4_asoomoutput.bag')
-    am.show_color()
+    rospy.spin()
