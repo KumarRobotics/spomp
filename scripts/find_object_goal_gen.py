f8dc8280f47236fb64aec6cd8fc5e16eb1a3b598
Ian D. Miller <iandouglas96@gmail.com>
2023-03-11 14:05:59 -0500

    find_object_goal_gen now working with new spomp stack

--- scripts/find_object_goal_gen.py
+++ scripts/find_object_goal_gen.py
@@ -13,15 +13,16 @@ class FindObjectGoalGen:
         self.last_map_stamp_ = 0
         self.map_size_ = np.array([0., 0])
         self.scale_ = 0
+        self.class_list_ = [0, 6, 3]
 
         self.scale_ = rospy.get_param("~resolution", 2)
         self.merge_dist_ = rospy.get_param("~merge_dist", 10) * self.scale_
-        self.min_size_ = (rospy.get_param("~min_size", 3) * self.scale_) ** 2
-        self.region_size_ = rospy.get_param("~region_size", 10) * self.scale_
+        self.min_size_ = (rospy.get_param("~min_size", 2) * self.scale_) ** 2
+        self.region_size_ = rospy.get_param("~region_size", 7) * self.scale_
 
-        self.map_sub_ = rospy.Subscriber("/quadrotor/asoom/map", GridMap, self.map_cb)
+        self.map_sub_ = rospy.Subscriber("~map", GridMap, self.map_cb)
         self.goal_viz_pub_ = rospy.Publisher("~goal_viz", Image, queue_size=1)
-        self.target_goals_pub_ = rospy.Publisher("/groundstation/target_goals", PoseArray, queue_size=1)
+        self.target_goals_pub_ = rospy.Publisher("~target_goals", PoseArray, queue_size=1)
 
     def world2img(self, world_pos):
         return ((-world_pos + self.origin_) * self.scale_ +
@@ -43,8 +44,9 @@ class FindObjectGoalGen:
 
         layer_ind = map_msg.layers.index("semantics")
         sem_layer = map_msg.data[layer_ind]
-        map_img = np.array(sem_layer.data).astype(np.uint8).reshape(
-                sem_layer.layout.dim[0].size, sem_layer.layout.dim[1].size, -1)
+        map_img = np.nan_to_num(np.array(sem_layer.data), nan=255).astype(np.uint8).reshape(
+                sem_layer.layout.dim[0].size, sem_layer.layout.dim[1].size)
+        map_img = map_img.transpose()
 
         self.scale_ = 1./map_msg.info.resolution
         self.origin_ = np.array([center.x, center.y])
@@ -52,8 +54,8 @@ class FindObjectGoalGen:
 
         self.find_goals(map_img)
 
-    def compute_roadmap(self, map_img):
-        road_map = np.logical_or(map_img == 0, map_img == 6)
+    def compute_dist_map(self, map_img, cls_list):
+        road_map = np.isin(map_img, cls_list)
         # 0 everywhere we have obstacle
         obstacle_map_inv = np.logical_or(road_map, map_img == 255) 
         road_map = cv2.morphologyEx(road_map.astype(np.uint8), cv2.MORPH_CLOSE, 
@@ -90,28 +92,32 @@ class FindObjectGoalGen:
         
         return blob_centers
 
-    def choose_goals(self, blob_centers, distance_map):
+    def choose_goals(self, blob_centers, map_img):
         goals = np.zeros((0, 2)) 
         for goal in blob_centers:
-            # Ok, find safest location in goal region
+            # compute search square
             goal_region_min_corner = np.maximum(0, self.world2img(goal) - 
-                    int(self.region_size_ / self.scale_))
-            goal_region_max_corner = np.minimum(np.array(distance_map.shape), 
-                    self.world2img(goal) + int(self.region_size_ / self.scale_))
+                    int(self.region_size_))
+            goal_region_max_corner = np.minimum(np.array(map_img.shape[:2]), 
+                    self.world2img(goal) + int(self.region_size_))
 
-            local_dist_map = distance_map[goal_region_min_corner[0]:goal_region_max_corner[0],
-                                          goal_region_min_corner[1]:goal_region_max_corner[1]]
-            best_pt = np.unravel_index(local_dist_map.argmax(), local_dist_map.shape)
-            best_pt += goal_region_min_corner
-
-            goals = np.vstack([goals, self.img2world(best_pt)[None,:]])
+            for cls_num in range(1, len(self.class_list_)+1):
+                distance_map = self.compute_dist_map(map_img, self.class_list_[:cls_num])
+                # Ok, find safest location in goal region
+                local_dist_map = distance_map[goal_region_min_corner[0]:goal_region_max_corner[0],
+                                              goal_region_min_corner[1]:goal_region_max_corner[1]]
+                best_pt = np.unravel_index(local_dist_map.argmax(), local_dist_map.shape)
+                if local_dist_map[best_pt[0], best_pt[1]] > 0:
+                    # the best point is in a valid region
+                    best_pt += goal_region_min_corner
+                    goals = np.vstack([goals, self.img2world(best_pt)[None,:]])
+                    break
 
         return goals
 
     def find_goals(self, map_img):
-        roadmap = self.compute_roadmap(map_img)
         blob_centers = self.detect_roi(map_img) 
-        goals = self.choose_goals(blob_centers, roadmap)
+        goals = self.choose_goals(blob_centers, map_img)
         rospy.loginfo(f"Found {goals.shape[0]} goals")
         self.pub_viz(map_img, blob_centers, goals)
         self.pub_goals(goals)
@@ -129,8 +135,7 @@ class FindObjectGoalGen:
         self.target_goals_pub_.publish(goals_msg)
 
     def pub_viz(self, viz_img, blob_centers, goals):
-        viz = viz_img.copy()
-        viz = cv2.cvtColor(viz*20, cv2.COLOR_GRAY2BGR)
+        viz = cv2.cvtColor(viz_img.copy()*20, cv2.COLOR_GRAY2BGR)
         viz[viz[:,:,0] == 4*20, :] = np.array([255, 255, 0])
         for blob_w in blob_centers:
             blob = self.world2img(blob_w)
